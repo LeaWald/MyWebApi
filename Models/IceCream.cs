d36012ad6c32b615e7693ee9391f705ca18213e3
LeaWald <lea0548591269@gmail.com>
2024-12-10 21:35:03 +0200

    Miri Apfeldorfer and Lea Waldman Project

---
+++ Models/IceCream.cs
@@ -0,0 +1,11 @@
+namespace MyWebApi.Models;
+
+public class IceCream
+{
+    public string Name { get; set; }
+     public int id { get; set; }
+    public int userId {get;set;}
+    public double price {get;set;}
+    public bool isDairy {get; set;}
+
+}
